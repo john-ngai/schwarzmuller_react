7e230fdb4feb42b74cfe1870c147d1214ae8220a
mrjohnming <john.ngai@hotmail.com>
2022-06-24 13:28:13 -0400

    Pass and display mock data from root to the ExpenseItem component

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -1,10 +1,41 @@
-import ExpenseItem from './components/ExpenseItem';
+import ExpenseItem from "./components/ExpenseItem";
 
 export default function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet paper",
+      amount: 28.25,
+      date: new Date(2022, 5, 13),
+    },
+    {
+      id: "e2",
+      title: "Ribeye steaks",
+      amount: 52.74,
+      date: new Date(2022, 5, 18),
+    },
+    {
+      id: "e3",
+      title: "Bottled water",
+      amount: 9.14,
+      date: new Date(2022, 6, 4),
+    },
+    {
+      id: "e4",
+      title: "Charcoal",
+      amount: 14.97,
+      date: new Date(2022, 6, 24),
+    },
+  ];
+
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem />
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      />
     </div>
   );
 }
