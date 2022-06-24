c9179b30bc42e7f5753235483f62872d0631cfba
mrjohnming <john.ngai@hotmail.com>
2022-06-24 13:55:46 -0400

    Create an Expenses component

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -1,28 +1,29 @@
-import ExpenseItem from "./components/ExpenseItem";
+import ExpenseItem from './components/ExpenseItem';
+import Expenses from './components/Expenses'
 
 export default function App() {
   const expenses = [
     {
-      id: "e1",
-      title: "Toilet paper",
+      id: 'e1',
+      title: 'Toilet paper',
       amount: 28.25,
       date: new Date(2022, 5, 13),
     },
     {
-      id: "e2",
-      title: "Ribeye steaks",
+      id: 'e2',
+      title: 'Ribeye steaks',
       amount: 52.74,
       date: new Date(2022, 5, 18),
     },
     {
-      id: "e3",
-      title: "Bottled water",
+      id: 'e3',
+      title: 'Bottled water',
       amount: 9.14,
       date: new Date(2022, 6, 4),
     },
     {
-      id: "e4",
-      title: "Charcoal",
+      id: 'e4',
+      title: 'Charcoal',
       amount: 14.97,
       date: new Date(2022, 6, 24),
     },
@@ -36,6 +37,7 @@ export default function App() {
         amount={expenses[0].amount}
         date={expenses[0].date}
       />
+      <Expenses />
     </div>
   );
 }
---
+++ 01-starting-setup/src/components/Expenses.js
@@ -0,0 +1,5 @@
+export default function Expenses() {
+  return (
+    <div className="expenses">New Component - Expenses</div>
+  );
+}
