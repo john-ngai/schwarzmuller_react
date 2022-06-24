c91fa674537f06589c1556a45709c75139a78241
mrjohnming <john.ngai@hotmail.com>
2022-06-24 14:45:34 -0400

    Replace parent div with <Card />

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,3 +1,4 @@
+import Card from "./Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
@@ -5,12 +6,12 @@ export default function ExpenseItem(props) {
   const { title, amount, date } = props;
 
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={date} />
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">${amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
