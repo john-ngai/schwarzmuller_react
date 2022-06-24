5e8c1c96b6e0ee049b6e4e62c9f94558348abf26
mrjohnming <john.ngai@hotmail.com>
2022-06-24 13:08:05 -0400

    Add initial JSX content

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,5 +1,13 @@
+import './ExpenseItem.css';
+
 export default function ExpenseItem() {
   return(
-    <h2>Expense Item!</h2>
+    <div className='expense-item'>
+      <div>24 June 2022</div>
+      <div className='expense-item__description'>
+        <h2>Car Insurance</h2>
+        <div className='expense-item__price'>$311.57</div>
+      </div>
+    </div>
   );
 }
