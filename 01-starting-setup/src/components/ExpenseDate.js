aef73fb40bce998ca01d920e5532eae2d5a54ced
mrjohnming <john.ngai@hotmail.com>
2022-06-24 13:45:28 -0400

    Move the date logic into a ExpenseDate component

---
+++ 01-starting-setup/src/components/ExpenseDate.js
@@ -0,0 +1,14 @@
+export default function ExpenseDate(props) {
+  const { date } = props;
+  const day = date.toLocaleString("en-US", { day: "2-digit" });
+  const month = date.toLocaleString("en-US", { month: "long" });
+  const year = date.getFullYear();
+
+  return (
+    <div>
+      <div>{day}</div>
+      <div>{month}</div>
+      <div>{year}</div>
+    </div>
+  );
+}
