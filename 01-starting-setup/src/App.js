07b0895c5244c25056b0dfd01c05bad780ef6616
mrjohnming <john.ngai@hotmail.com>
2022-06-24 14:06:05 -0400

    Update expenses data

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -6,25 +6,25 @@ export default function App() {
       id: "e1",
       title: "Toilet paper",
       amount: 28.25,
-      date: new Date(2022, 5, 13),
+      date: new Date(2022, 4, 13),
     },
     {
       id: "e2",
       title: "Ribeye steaks",
       amount: 52.74,
-      date: new Date(2022, 5, 18),
+      date: new Date(2022, 4, 18),
     },
     {
       id: "e3",
       title: "Bottled water",
       amount: 9.14,
-      date: new Date(2022, 6, 4),
+      date: new Date(2022, 5, 4),
     },
     {
       id: "e4",
       title: "Charcoal",
       amount: 14.97,
-      date: new Date(2022, 6, 24),
+      date: new Date(2022, 5, 24),
     },
   ];
 
