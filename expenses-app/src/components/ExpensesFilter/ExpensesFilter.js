1372d836647d278ea975279309a2a98c20f79c9b
mrjohnming <john.ngai@hotmail.com>
2022-06-28 12:10:56 -0400

    Create function - dropdownChangeHandler

--- expenses-app/src/components/ExpensesFilter/ExpensesFilter.js
+++ expenses-app/src/components/ExpensesFilter/ExpensesFilter.js
@@ -1,11 +1,18 @@
 import './ExpensesFilter.css';
 
-export default function ExpensesFilter() {
+export default function ExpensesFilter(props) {
+  const { filteredYear, onChangeFilter } = props;
+
+  const dropdownChangeHandler = event => {
+    const selectedYear = event.target.value;
+    onChangeFilter(selectedYear);
+  }
+
   return (
     <div className='expenses-filter'>
       <div className='expenses-filter__control'>
         <label>Filter by year</label>
-        <select>
+        <select value={filteredYear} onChange={dropdownChangeHandler}>
           <option value='2022'>2022</option>
           <option value='2021'>2021</option>
           <option value='2020'>2020</option>
