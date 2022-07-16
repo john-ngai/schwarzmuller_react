8b64bf99e95fa6047ee1e58e3dbd5d8c7b751f67
mrjohnming <john.ngai@hotmail.com>
2022-07-16 16:34:33 -0400

    Rename prop from expenses into items

--- expenses-app/src/App.js
+++ expenses-app/src/App.js
@@ -37,7 +37,7 @@ export default function App() {
   return (
     <div>
       <NewExpense onAddExpense={addExpenseHandler} />
-      <Expenses expenses={expenses} />
+      <Expenses items={expenses} />
     </div>
   );
 }
--- expenses-app/src/components/Expenses/Expenses.js
+++ expenses-app/src/components/Expenses/Expenses.js
@@ -4,7 +4,7 @@ import ExpensesFilter from '../ExpensesFilter/ExpensesFilter';
 import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
 
-export default function Expenses({ expenses }) {
+export default function Expenses({ items }) {
   const [ filteredYear, setFilteredYear ] = useState('2022');
   
   const filterChangeHandler = selectedYear => {
@@ -17,25 +17,28 @@ export default function Expenses({ expenses }) {
         onChangeFilter={filterChangeHandler}
         filteredYear={filteredYear}
       />
+
+      {}
+
       <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
+        title={items[0].title}
+        amount={items[0].amount}
+        date={items[0].date}
       />
       <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
+        title={items[1].title}
+        amount={items[1].amount}
+        date={items[1].date}
       />
       <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
+        title={items[2].title}
+        amount={items[2].amount}
+        date={items[2].date}
       />
       <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
+        title={items[3].title}
+        amount={items[3].amount}
+        date={items[3].date}
       />
     </Card>
   );
