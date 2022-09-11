b3acbd20821a891323963b60d291f92652c351d6
mrjohnming <john.ngai@hotmail.com>
2022-09-11 11:20:03 -0400

    Remove legacy code

--- 02_course-goals-app/src/components/UI/Button/Button.js
+++ 02_course-goals-app/src/components/UI/Button/Button.js
@@ -21,12 +21,4 @@ const Button = styled.button`
   }
 `;
 
-// const Button = props => {
-//   return (
-//     <button type={props.type} className="button" onClick={props.onClick}>
-//       {props.children}
-//     </button>
-//   );
-// };
-
 export default Button;
